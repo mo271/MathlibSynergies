bb469ca9a38235c56daabdddea81ff4df8faa570
Moritz Firsching <firsching@google.com>
2024-03-21 11:20:35 +0100

    more problems and solutions

--- MathlibSynergies/Basic.lean
+++ MathlibSynergies/Basic.lean
@@ -21,11 +21,26 @@ theorem two_mul_3_add_5 : 2 * (3 + 5) = 2 * 3 + 2 * 5 := by
 theorem two_mul_3_add_5' : 2 * (3 + 5) = 2 * 3 + 2 * 5 := mul_add 2 3 5
 
 theorem square_diff_nonneg (a b : ℤ) : 0 ≤ a ^ 2 - 2 * a * b + b ^ 2 := by
-  sorry
+  have :  a ^ 2 - 2 * a * b + b ^ 2 = (a - b) ^ 2 := by ring
+  rw [this]
+  exact sq_nonneg (a - b)
+
+
+theorem square_diff_nonneg' (a b : ℤ) : 0 ≤ a ^ 2 - 2 * a * b + b ^ 2 := by
+  convert sq_nonneg (a - b)
+  ring
 
 -- hint: use `Nat.ofDigits_mod 10 3 (Nat.digits 10 n)`
 theorem three_divisibility_rule (n : ℕ) : 3 ∣ n ↔ 3 ∣ (Nat.digits 10 n).sum := by
-  sorry
+  have h10 : 10 % 3 = 1 := by rfl
+  have := Nat.ofDigits_mod 10 3 (Nat.digits 10 n)
+  rw [← Nat.ofDigits_digits 10 n]
+  rw [Nat.dvd_iff_mod_eq_zero]
+  rw [this]
+  rw [h10]
+  rw [Nat.ofDigits_one]
+  rw [Nat.dvd_iff_mod_eq_zero]
+  rw [Nat.ofDigits_digits 10 n]
 
 theorem euclid : ∀ n : ℕ, ∃ p : ℕ, p.Prime ∧ n < p := by
   sorry
