e9597f95a68cc80bd07b5267b577bfa2d43fd29c
Moritz Firsching <firsching@google.com>
2024-03-21 13:54:23 +0100

    even example

--- MathlibSynergies/CommonPitfalls.lean
+++ MathlibSynergies/CommonPitfalls.lean
@@ -17,3 +17,12 @@ import Mathlib
 #eval Nat.digits 123456 10
 
 #eval 1/(1 - 2) + 1 = 1
+
+example : ¬ Even 3 := by
+  rw [← Nat.odd_iff_not_even]
+  use 1
+  norm_num
+
+example : Even (3 : ℚ) := by
+  use 3 / 2
+  rfl
